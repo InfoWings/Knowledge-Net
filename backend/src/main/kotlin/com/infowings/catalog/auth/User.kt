e9ed98de419ada50b2271f02092e2c745c02baa9
artemijbezguzikov <artemii.bezguzikov@gmail.com>
2018-02-12 18:15:20 +0300

    remove postgresql from project

--- backend/src/main/kotlin/com/infowings/catalog/auth/User.kt
+++ backend/src/main/kotlin/com/infowings/catalog/auth/User.kt
@@ -1,17 +1,25 @@
 package com.infowings.catalog.auth
 
 import com.infowings.common.UserRole
-import org.springframework.data.jpa.repository.JpaRepository
-import javax.persistence.*
+import org.springframework.stereotype.Service
 
-@Entity
-@Table(name = "userentity", indexes = [Index(name = "username_ind", columnList = "username", unique = true)])
-data class UserEntity(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long = 0,
-                      @Column(name = "username")
+val admin = UserEntity(0, "admin", "admin", UserRole.ADMIN)
+val user = UserEntity(1, "user", "user", UserRole.USER)
+val poweredUser = UserEntity(2, "powereduser", "powereduser", UserRole.POWERED_USER)
+
+data class UserEntity(var id: Long,
                       var username: String,
                       var password: String,
-                      @Enumerated(EnumType.STRING) var role: UserRole)
+                      var role: UserRole)
 
-interface UserRepository : JpaRepository<UserEntity, Long> {
-    fun findByUsername(username: String): UserEntity?
+@Service
+class UserRepository {
+    fun findByUsername(username: String): UserEntity? {
+        return when (username) {
+            admin.username -> admin
+            user.username -> user
+            poweredUser.username -> poweredUser
+            else -> throw IllegalStateException("There are no such user")
+        }
+    }
 }
