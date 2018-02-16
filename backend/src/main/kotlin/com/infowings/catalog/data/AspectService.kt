7668aa635456b905fe5b51f3652e27de3c420dad
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2018-02-16 12:11:06 +0300

    KS-4 Внешний API - Просмотр/редактирование аспекта

--- backend/src/main/kotlin/com/infowings/catalog/data/AspectService.kt
+++ backend/src/main/kotlin/com/infowings/catalog/data/AspectService.kt
@@ -9,15 +9,14 @@ import com.orientechnologies.orient.core.sql.executor.OResultSet
 class AspectService(private val database: OrientDatabase, private val measureService: MeasureService) {
 
     private fun save(name: String, measureUnit: Measure<*>?, baseType: BaseType?): Aspect {
-        val save: OVertex = transaction(database) { db ->
-            val statement = "SELECT FROM $MEASURE_VERTEX where name = ? ";
-            val rs: OResultSet = db.query(statement, measureUnit.toString());
-            val measureVertex: OVertex? = measureUnit?.name?.let { measureService.findMeasure(it, db) }
+        val save: OVertex = transaction(database) { session ->
 
-            val aspectVertex: OVertex = db.newVertex(ASPECT_CLASS)
+            val measureVertex: OVertex? = measureUnit?.name?.let { measureService.findMeasure(it, session) }
+            val aspectVertex: OVertex = session.newVertex(ASPECT_CLASS)
 
             aspectVertex["name"] = name
             aspectVertex["baseType"] = baseType?.name ?: measureUnit?.baseType?.name
+            aspectVertex["measure"] = measureUnit?.name
             measureVertex?.let { aspectVertex.addEdge(it, MEASURE_ASPECT_CLASS) }
 
             return@transaction aspectVertex.save()
@@ -36,7 +35,7 @@ class AspectService(private val database: OrientDatabase, private val measureSer
         if (findByName(name) != null)
             throw AspectAlreadyExist
 
-        val measure: Measure<*>? =  GlobalMeasureMap[measureName]
+        val measure: Measure<*>? = GlobalMeasureMap[measureName]
 
         val baseType: BaseType? = when {
             baseTypeString != null -> BaseType.restoreBaseType(baseTypeString)
@@ -44,7 +43,7 @@ class AspectService(private val database: OrientDatabase, private val measureSer
             else -> null
         }
 
-        if (baseType != null && measure!= null && baseType != measure.baseType)
+        if (baseType != null && measure != null && baseType != measure.baseType)
             throw IllegalArgumentException("Base type and measure base type should be the same")
 
         return save(name, measure, baseType)
@@ -60,7 +59,7 @@ class AspectService(private val database: OrientDatabase, private val measureSer
                 return@transaction Aspect(
                     id = vertex.id,
                     name = vertex.name,
-                    measureUnit = GlobalMeasureMap[vertex.measureName],
+                    measureUnit = vertex.measureName,
                     baseType = vertex.baseType,
                     domain = vertex.baseType?.let { OpenDomain(it) }
                 )
@@ -75,8 +74,8 @@ class AspectService(private val database: OrientDatabase, private val measureSer
         get() = BaseType.restoreBaseType(this["baseType"])
     private val OVertex.name: String
         get() = this["name"]
-    private val OVertex.measureName: String?
-        get() = this.getEdges(ODirection.OUT, MEASURE_ASPECT_CLASS).firstOrNull()?.to?.name
+    private val OVertex.measureName: Measure<*>?
+        get() = GlobalMeasureMap[this["measure"]]
     private val OVertex.id: String
         get() = identity.toString()
 }
