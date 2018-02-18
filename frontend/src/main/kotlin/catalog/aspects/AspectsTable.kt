2be2113637184fc5d16ad03b9b04310b5d7b6b1a
kochkr <kochkar.roman@gmail.com>
2018-02-18 23:59:21 +0300

    KS-7: ReactTable integration v1

--- frontend/src/main/kotlin/catalog/aspects/AspectsTable.kt
+++ frontend/src/main/kotlin/catalog/aspects/AspectsTable.kt
@@ -1,17 +1,55 @@
 package catalog.aspects
 
-import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
-import wrappers.ReactTable
+import react.*
+import react.dom.span
+import wrappers.table.*
+import kotlin.js.json
 
+fun headerComponent(columnName: String) = rFunction<RTableRendererProps>("AspectHeader") {
+    span {
+        +columnName
+    }
+}
+
+val cellComponent = rFunction<RTableRendererProps>("LoggingCell") { rTableRendererProps ->
+    +rTableRendererProps.value.toString()
+}
+
+fun aspectColumn(accessor: String, header: RClass<RTableRendererProps>, cell: RClass<RTableRendererProps>) =
+        RTableColumnDescriptor().apply {
+            this.accessor = accessor
+            this.Header = header
+            this.Cell = cell
+        }
+
+private val aspectData = arrayOf(
+        json(
+                "category" to "Category",
+                "measureUnit" to "measureUnit",
+                "type" to "type",
+                "domain" to "domain"
+        )
+)
 
 class AspectsTable : RComponent<RProps, RState>() {
 
+
     override fun RBuilder.render() {
         ReactTable {
+            attrs {
+                columns = arrayOf(
+                        aspectColumn("category", headerComponent("Category"), cellComponent),
+                        aspectColumn("measureUnit", headerComponent("Measure Unit"), cellComponent),
+                        aspectColumn("type", headerComponent("Type"), cellComponent),
+                        aspectColumn("domain", headerComponent("Domain"), cellComponent)
+                )
+                data = aspectData
+                showPagination = false
+                minRows = 2
+                sortable = false
+                showPageJump = false
 
+            }
         }
     }
 }
---
+++ frontend/src/main/kotlin/wrappers/table/Import.kt
@@ -0,0 +1,10 @@
+@file:JsModule("react-table")
+
+package wrappers.table
+
+import react.RClass
+
+@JsName("default")
+external val ReactTable: RClass<RTableProps> = definedExternally
+external val ReactTableDefaults: RTableProps = definedExternally
+
