71daa1ca539200d37bc952705f2354fca48d088b
yaburtsev <YBurtsev@luxoft.com>
2018-02-19 16:17:24 +0300

    KS-12-base-rest-api refactoring

--- frontend/src/main/kotlin/utils/ServerApi.kt
+++ frontend/src/main/kotlin/utils/ServerApi.kt
@@ -14,12 +14,28 @@ import kotlin.browser.localStorage
 import kotlin.browser.window
 import kotlin.js.json
 
-private val defaultHeaders = json(
-    "Accept" to "application/json",
-    "Content-Type" to "application/json;charset=UTF-8"
-)
+private const val POST = "POST"
+private const val GET = "GET"
 
-suspend fun request(method: String, url: String, body: dynamic, headers: dynamic = defaultHeaders): Response =
+private const val AUTH_ACCESS_TOKEN = "auth-access-token"
+private const val AUTH_REFRESH_TOKEN = "auth-refresh-token"
+private const val AUTH_ROLE = "auth-role"
+
+suspend fun post(url: String, body: dynamic) = authorizedRequest(POST, url, body)
+
+suspend fun get(url: String, body: dynamic = null) = authorizedRequest(GET, url, body)
+
+private suspend fun authorizedRequest(method: String, url: String, body: dynamic): Response {
+    var response = request(method, url, body, authorizationHeaders)
+
+    if (!response.ok) {
+        response = refreshAndRepeat(method, url, body, response)
+    }
+
+    return response
+}
+
+private suspend fun request(method: String, url: String, body: dynamic, headers: dynamic = defaultHeaders): Response =
     window.fetch(url, object : RequestInit {
         override var method: String? = method
         override var body: dynamic = body
@@ -27,23 +43,39 @@ suspend fun request(method: String, url: String, body: dynamic, headers: dynamic
         override var headers: dynamic = headers
     }).await()
 
-suspend fun post(url: String, body: dynamic) = request("POST", url, body, authorizationHeaders)
+private val authorizationHeaders = json(
+    "x-access-authorization" to "Bearer ${localStorage[AUTH_ACCESS_TOKEN]}",
+    "x-refresh-authorization" to "Bearer ${localStorage[AUTH_REFRESH_TOKEN]}"
+)
 
-suspend fun get(url: String, body: dynamic = null) = request("GET", url, body, authorizationHeaders)
+private val defaultHeaders = json(
+    "Accept" to "application/json",
+    "Content-Type" to "application/json;charset=UTF-8"
+)
 
-suspend fun <T> requestAndParseResult(method: String, url: String, body: dynamic, parse: (dynamic) -> T): T {
-    val response = request(method, url, body, authorizationHeaders)
-    return parse(response.json().await())
+private suspend fun refreshAndRepeat(method: String, url: String, body: dynamic, oldResponse: Response): Response {
+    val isRefreshed = refresh()
+    if (isRefreshed) {
+        return request(method, url, body, authorizationHeaders)
+    }
+    removeTokenInfo()
+    window.location.replace("/")
+    return oldResponse
 }
 
 suspend fun <T> postAndParseResult(url: String, body: dynamic, parse: (dynamic) -> T): T =
-    requestAndParseResult("POST", url, body, parse)
+    requestAndParseResult(POST, url, body, parse)
 
 suspend fun <T> getAndParseResult(url: String, body: dynamic, parse: (dynamic) -> T): T =
-    requestAndParseResult("GET", url, body, parse)
+    requestAndParseResult(GET, url, body, parse)
+
+private suspend fun <T> requestAndParseResult(method: String, url: String, body: dynamic, parse: (dynamic) -> T): T {
+    val response = authorizedRequest(method, url, body)
+    return parse(response.json().await())
+}
 
 suspend fun login(body: UserDto): Boolean {
-    val response = request("POST", "/api/access/signIn", JSON.stringify(body))
+    val response = request(POST, "/api/access/signIn", JSON.stringify(body))
     if (response.ok) {
         val isParsed = parseToken(response)
         return isParsed
@@ -55,36 +87,13 @@ private suspend fun parseToken(response: Response): Boolean {
     try {
         val jwtToken = JSON.parse<JwtToken>(response.text().await())
         console.log("refresh: $jwtToken")
-        localStorage["auth-access-token"] = jwtToken.accessToken
-        localStorage["auth-refresh-token"] = jwtToken.refreshToken
-        localStorage["auth-role"] = jwtToken.role.name
+        localStorage[AUTH_ACCESS_TOKEN] = jwtToken.accessToken
+        localStorage[AUTH_REFRESH_TOKEN] = jwtToken.refreshToken
+        localStorage[AUTH_ROLE] = jwtToken.role.name
         return true
     } catch (e: Exception) {
         return false
     }
-}
-
-suspend fun getResponseText(url: String): String {
-    var response = get(url)
-    if (!response.ok) {
-        response = refreshAndGet(url) ?: response
-    }
-    return response.text().await()
-}
-
-private val authorizationHeaders = json(
-    "x-access-authorization" to "Bearer ${localStorage["auth-access-token"]}",
-    "x-refresh-authorization" to "Bearer ${localStorage["auth-refresh-token"]}"
-)
-
-private suspend fun refreshAndGet(url: String): Response? {
-    val isRefreshed = refresh()
-    if (isRefreshed) {
-        return get(url)
-    }
-    removeTokenInfo()
-    window.location.replace("/")
-    return null
 }
 
 private suspend fun refresh(): Boolean {
@@ -97,7 +106,7 @@ private suspend fun refresh(): Boolean {
 }
 
 private fun removeTokenInfo() {
-    localStorage.removeItem("auth-access-token")
-    localStorage.removeItem("auth-refresh-token")
-    localStorage.removeItem("auth-role")
+    localStorage.removeItem(AUTH_ACCESS_TOKEN)
+    localStorage.removeItem(AUTH_REFRESH_TOKEN)
+    localStorage.removeItem(AUTH_ROLE)
 }
