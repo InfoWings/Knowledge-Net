b4d01b7ff0c57d853c343e30971d5cd215b6002a
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2018-02-20 19:59:28 +0300

    KS-4 Внешний API - Просмотр/редактирование аспекта

--- backend/src/main/kotlin/com/infowings/catalog/data/Aspect.kt
+++ backend/src/main/kotlin/com/infowings/catalog/data/Aspect.kt
@@ -1,5 +1,7 @@
 package com.infowings.catalog.data
 
+import com.infowings.catalog.common.AspectData
+import com.infowings.catalog.common.AspectPropertyData
 import com.infowings.catalog.common.BaseType
 import com.infowings.catalog.common.Measure
 
@@ -12,15 +14,21 @@ enum class AspectPropertyPower {
  * Аспект - https://iwings.atlassian.net/wiki/spaces/CHR/pages/219217938
  */
 data class Aspect(
-        val id: String,
-        val name: String,
-        val measure: Measure<*>?,
-        val domain: AspectDomain? = OpenDomain(measure?.baseType
-                ?: throw IllegalArgumentException("Measure unit cannot be null if no base type specified")),
-        val baseType: BaseType? = measure?.baseType
-                ?: throw IllegalArgumentException("Measure unit cannot be null if no base type specified"),
-        val properties: List<AspectProperty> = emptyList()
-)
+    val id: String,
+    val name: String,
+    val measure: Measure<*>?,
+    val domain: AspectDomain? = OpenDomain(measure?.baseType ?: throw IllegalArgumentException("Measure unit cannot be null if no base type specified")),
+    val baseType: BaseType? = measure?.baseType
+            ?: throw IllegalArgumentException("Measure unit cannot be null if no base type specified"),
+    val properties: List<AspectProperty> = emptyList()
+) {
+    fun toAspectData(): AspectData =
+        AspectData(id, name, measure?.name, domain.toString(), baseType?.name, properties.toAspectPropertyData())
+}
+
+fun List<Aspect>.toAspectData(): List<AspectData> = map { it.toAspectData() }
+fun List<AspectProperty>.toAspectPropertyData(): List<AspectPropertyData> = map { AspectPropertyData(it.id, it.name, it.aspect.id, it.power.name) }
+
 
 data class AspectProperty(
     val id: String,
--- backend/src/main/kotlin/com/infowings/catalog/external/AspectApi.kt
+++ backend/src/main/kotlin/com/infowings/catalog/external/AspectApi.kt
@@ -1,11 +1,9 @@
 package com.infowings.catalog.external
 
-import com.infowings.catalog.data.Aspect
-import com.infowings.catalog.data.AspectProperty
+import com.infowings.catalog.common.AspectData
 import com.infowings.catalog.data.AspectService
+import com.infowings.catalog.data.toAspectData
 import com.infowings.catalog.loggerFor
-import com.infowings.common.catalog.data.AspectData
-import com.infowings.common.catalog.data.AspectPropertyData
 import org.springframework.web.bind.annotation.*
 
 //todo: перехватывание exception и генерация внятных сообщений об ошибках наружу
@@ -38,12 +36,6 @@ class AspectApi(val aspectService: AspectService) {
         logger.debug("Get all aspects request")
         return aspectService.getAspects().toAspectData()
     }
-
-    private fun Aspect.toAspectData() = AspectData(id, name, measure?.name, domain.toString(), baseType?.name, properties.toAspectPropertyData())
-
-    private fun List<Aspect>.toAspectData(): List<AspectData> = map { it.toAspectData() }
-
-    private fun List<AspectProperty>.toAspectPropertyData(): List<AspectPropertyData> = map { AspectPropertyData(it.id, it.name, it.aspect.id, it.power.name) }
 
 }
 
