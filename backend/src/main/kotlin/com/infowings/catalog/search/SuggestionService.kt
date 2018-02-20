64356c82f9f2999902904c86263ec23d9a8b82a1
Sergey Zagrebelny <sboon1974@yandex.ru>
2018-02-20 20:01:33 +0300

    KS-10: remove ODocument

--- backend/src/main/kotlin/com/infowings/catalog/search/SuggestionService.kt
+++ backend/src/main/kotlin/com/infowings/catalog/search/SuggestionService.kt
@@ -3,11 +3,9 @@ package com.infowings.catalog.search
 import com.infowings.catalog.data.MEASURE_VERTEX
 import com.infowings.catalog.storage.OrientDatabase
 import com.infowings.catalog.storage.session
+import com.infowings.catalog.storage.toVertexOrNUll
 import com.orientechnologies.orient.core.record.OElement
 import com.orientechnologies.orient.core.record.OVertex
-import com.orientechnologies.orient.core.record.impl.ODocument
-import java.util.stream.Collectors
-import java.util.stream.Stream
 
 /**
  * Сервис поиска в OrientDB
@@ -15,28 +13,18 @@ import java.util.stream.Stream
 class SuggestionService(val database: OrientDatabase) {
 
     fun find(context: SearchContext, text: String): List<MeasureSuggestion> = session(database) {
-            findInDb(context, text)
-                    .map { toMeasureSuggestionDto(it) }
-                    .toList()
+        findInDb(context, text)
+                .map { toMeasureSuggestionDto(it) }
+                .toList()
     }
 
-    private fun toMeasureSuggestionDto(oElement: OElement?): MeasureSuggestion =
-            when (oElement) {
-                is OVertex -> vertexToMeasureSuggestionDto(oElement)
-                is ODocument -> vertexToMeasureSuggestionDto(oElement)
-                else -> throw IllegalStateException("Wrong measure unit $oElement")
-            }
-
-    private fun vertexToMeasureSuggestionDto(oVertex: OVertex): MeasureSuggestion =
+    private fun toMeasureSuggestionDto(oVertex: OVertex): MeasureSuggestion =
             MeasureSuggestion(oVertex.getProperty("name"))
 
-    private fun vertexToMeasureSuggestionDto(oDocument: ODocument): MeasureSuggestion =
-            MeasureSuggestion(oDocument.getProperty("name"))
-
-    private fun findInDb(context: SearchContext, text: String): Sequence<OElement> {
+    private fun findInDb(context: SearchContext, text: String): Sequence<OVertex> {
         val q = "SELECT FROM $MEASURE_VERTEX WHERE SEARCH_CLASS(?) = true"
         return database.query(q, "($text~) ($text*) (*$text*)") {
-            it.map { it.toElement() }
+            it.mapNotNull { it.toVertexOrNUll() }
         }
 
     }
