25018bdf0948732002c01530baf06ca84317001e
artemaliev <artem.aliev@gmail,com>
2018-03-05 15:09:27 +0300

    KS-30: Add SuggestionService.findAspectNoCycle() method Remove parents* from suggested results.

--- backend/src/main/kotlin/com/infowings/catalog/search/SuggestionService.kt
+++ backend/src/main/kotlin/com/infowings/catalog/search/SuggestionService.kt
@@ -7,6 +7,7 @@ import com.infowings.catalog.common.Measure
 import com.infowings.catalog.data.MEASURE_VERTEX
 import com.infowings.catalog.data.OpenDomain
 import com.infowings.catalog.storage.*
+import com.orientechnologies.orient.core.id.ORecordId
 import com.orientechnologies.orient.core.record.OVertex
 
 /**
@@ -33,6 +34,14 @@ class SuggestionService(val database: OrientDatabase) {
         }
     }
 
+    fun findAspectNoCycle(parentAspectId: String, text: String): List<AspectData> = session(database) {
+        val q = "select * from $ASPECT_CLASS where SEARCH_CLASS(?) = true and " +
+                "@rid not in (select @rid from (traverse in(\"$ASPECT_ASPECTPROPERTY_EDGE\").in() FROM ?))"
+
+        return database.query(q, "($text~) ($text*) (*$text*)", ORecordId(parentAspectId)) {
+            it.mapNotNull { it.toVertexOrNUll()?.toAspectData() }.toList()
+        }
+    }
 
     private fun OVertex.toMeasure() = GlobalMeasureMap[this["name"]]
 
