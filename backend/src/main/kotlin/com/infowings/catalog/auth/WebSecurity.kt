91de0bc651a6a4b65805857587777995f7b59ad1
yaburtsev <YBurtsev@luxoft.com>
2018-03-14 13:35:25 +0300

    KS-32-disable-jsessionid-cookie set session creation policy to stateless

--- backend/src/main/kotlin/com/infowings/catalog/auth/WebSecurity.kt
+++ backend/src/main/kotlin/com/infowings/catalog/auth/WebSecurity.kt
@@ -10,14 +10,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 
 
 @EnableWebSecurity
-class WebSecurity() : WebSecurityConfigurerAdapter() {
+class WebSecurity : WebSecurityConfigurerAdapter() {
 
     @Bean
-    fun objectMapperBuilder(): Jackson2ObjectMapperBuilder = Jackson2ObjectMapperBuilder().modulesToInstall(KotlinModule())
+    fun objectMapperBuilder(): Jackson2ObjectMapperBuilder =
+        Jackson2ObjectMapperBuilder().modulesToInstall(KotlinModule())
 
     @Autowired
     lateinit var env: Environment
@@ -29,22 +31,23 @@ class WebSecurity() : WebSecurityConfigurerAdapter() {
     lateinit var jwtService: JWTService
 
     override fun configure(http: HttpSecurity?) {
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         val jWTAuthorizationFilter = JWTAuthorizationFilter(authenticationManager(), env, jwtService)
 
         http?.let {
             it.csrf().disable().authorizeRequests()
-                    .antMatchers("/api/access/signIn", "/api/access/refresh").permitAll()
-                    .antMatchers("/api/admin/**").hasAuthority(UserRole.ADMIN.name)
-                    .antMatchers("/api/powereduser/**").hasAuthority(UserRole.POWERED_USER.name)
-                    .antMatchers("/api/user/**").hasAuthority(UserRole.USER.name)
-                    .antMatchers("/api/aspect/**").hasAuthority(UserRole.POWERED_USER.name)
-                    .antMatchers("/api/aspect/**").hasAuthority(UserRole.ADMIN.name)
-                    .antMatchers("/api/measure/**").hasAuthority(UserRole.POWERED_USER.name)
-                    .antMatchers("/api/measure/**").hasAuthority(UserRole.ADMIN.name)
-                    .anyRequest().authenticated()
-                    .and()
-                    .addFilter(jWTAuthorizationFilter)
+                .antMatchers("/api/access/signIn", "/api/access/refresh").permitAll()
+                .antMatchers("/api/admin/**").hasAuthority(UserRole.ADMIN.name)
+                .antMatchers("/api/powereduser/**").hasAuthority(UserRole.POWERED_USER.name)
+                .antMatchers("/api/user/**").hasAuthority(UserRole.USER.name)
+                .antMatchers("/api/aspect/**").hasAuthority(UserRole.POWERED_USER.name)
+                .antMatchers("/api/aspect/**").hasAuthority(UserRole.ADMIN.name)
+                .antMatchers("/api/measure/**").hasAuthority(UserRole.POWERED_USER.name)
+                .antMatchers("/api/measure/**").hasAuthority(UserRole.ADMIN.name)
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(jWTAuthorizationFilter)
         }
     }
 
