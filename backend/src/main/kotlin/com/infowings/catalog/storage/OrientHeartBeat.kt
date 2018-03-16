279fe0a136dc52fd0e724d1d5ebd6ab656a1d3fa
Sergey Reznick <sr@air-consulting.ru>
2018-03-16 12:42:46 +0300

    fixes after review

--- backend/src/main/kotlin/com/infowings/catalog/storage/OrientHeartBeat.kt
+++ backend/src/main/kotlin/com/infowings/catalog/storage/OrientHeartBeat.kt
@@ -3,26 +3,27 @@ package com.infowings.catalog.storage
 import com.infowings.catalog.loggerFor
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+import java.util.concurrent.TimeUnit
 
 /**
- * Периодически шлем запросы, чтобы сеанс не закрывался
+ * Периодически шлем запросы, чтобы сессия не закрывалась
  */
 class OrientHeartBeat(database: OrientDatabase, seconds: Int) {
     init {
         val logger = loggerFor<OrientDatabase>()
 
-        val period = seconds * 1000;
+        val period = TimeUnit.SECONDS.toMillis(seconds.toLong());
 
         logger.info("Initializing orient heart beat...")
 
         launch {
-            while (true) {
+            while (isActive) {
                 delay(period)
-                val res = transaction(database) {
+                val res = session(database) {
                     val query = "SELECT * from User where username = ?"
                     it.query(query, "username")
                 }
-                logger.info("heart beat result result: ${res}")
+                logger.info("heart beat result: ${res}")
             }
         }
 
