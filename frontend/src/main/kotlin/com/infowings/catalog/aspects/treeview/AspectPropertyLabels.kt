051a8519611468a36741615dcfeef6a451646afa
kochkr <kochkar.roman@gmail.com>
2018-03-23 19:13:46 +0300

    KS-50 - Split Property labels into several components

---
+++ frontend/src/main/kotlin/com/infowings/catalog/aspects/treeview/AspectPropertyLabels.kt
@@ -0,0 +1,58 @@
+package com.infowings.catalog.aspects.treeview
+
+import kotlinx.html.js.onClickFunction
+import react.RBuilder
+import react.dom.div
+import react.dom.span
+
+fun RBuilder.propertyLabel(
+        className: String?,
+        aspectPropertyName: String,
+        aspectPropertyCardinality: String,
+        aspectName: String,
+        aspectMeasure: String,
+        aspectDomain: String,
+        aspectBaseType: String,
+        onClick: () -> Unit
+) = div(classes = "aspect-tree-view--label${className?.let { " $className" } ?: ""}") {
+    attrs {
+        onClickFunction = {
+            it.stopPropagation()
+            it.preventDefault()
+            onClick()
+        }
+    }
+    span(classes = "aspect-tree-view--label-name") {
+        +(aspectName)
+    }
+    +":"
+    span(classes = "aspect-tree-view--label-property") {
+        +"("
+        span(classes = "aspect-tree-view--label-property-name") {
+            +aspectPropertyName
+        }
+        +":"
+        span(classes = "aspect-tree-view--label-property-cardinality") {
+            +aspectPropertyCardinality
+        }
+        +")"
+    }
+    +":"
+    span(classes = "aspect-tree-view--label-measure") {
+        +aspectMeasure
+    }
+    +":"
+    span(classes = "aspect-tree-view--label-domain") {
+        +aspectDomain
+    }
+    +":"
+    span(classes = "aspect-tree-view--label-base-type") {
+        +aspectBaseType
+    }
+}
+
+fun RBuilder.placeholderPropertyLabel(
+        className: String?
+) = div(classes = "aspect-tree-view--label${className?.let { " $className" } ?: ""}") {
+    +"(Enter new Aspect Property)"
+}
