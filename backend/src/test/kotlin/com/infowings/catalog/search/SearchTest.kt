cffc1c29e82d2dcca7668e290209e17e7f3a8eb5
Sergey Reznick <sr@air-consulting.ru>
2018-03-26 21:24:37 +0300

    Spring 2.0.0 migration

--- backend/src/test/kotlin/com/infowings/catalog/search/SearchTest.kt
+++ backend/src/test/kotlin/com/infowings/catalog/search/SearchTest.kt
@@ -12,7 +12,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.embedded.LocalServerPort
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
