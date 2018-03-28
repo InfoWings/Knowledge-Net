5f08485dba1fb763dae2e80b02ef1775a29201f7
yaburtsev <YBurtsev@luxoft.com>
2018-03-28 18:46:04 +0300

    KS-59-ref-books-remove add version and deleted fields

--- common/src/main/kotlin/com/infowings/catalog/common/ReferenceBook.kt
+++ common/src/main/kotlin/com/infowings/catalog/common/ReferenceBook.kt
@@ -7,14 +7,26 @@ import kotlinx.serialization.Transient
  * [root] is a fake component. Just ignore this
  * */
 @Serializable
-data class ReferenceBook(val name: String, val aspectId: String, val root: ReferenceBookItem) {
+data class ReferenceBook(
+    val name: String,
+    val aspectId: String,
+    val root: ReferenceBookItem,
+    val deleted: Boolean = false,
+    val version: Int = 0
+) {
     val id: String = root.id
     val children: List<ReferenceBookItem> = root.children
     operator fun get(child: String): ReferenceBookItem? = root[child]
 }
 
 @Serializable
-data class ReferenceBookItem(val id: String, val value: String, val children: List<ReferenceBookItem> = emptyList()) {
+data class ReferenceBookItem(
+    val id: String,
+    val value: String,
+    val children: List<ReferenceBookItem> = emptyList(),
+    val deleted: Boolean = false,
+    val version: Int = 0
+) {
     @Transient
     private val accessChildrenMap = children.map { it.value to it }.toMap()
 
@@ -25,7 +37,13 @@ data class ReferenceBookItem(val id: String, val value: String, val children: Li
 data class ReferenceBooksList(val books: List<ReferenceBook>)
 
 @Serializable
-data class ReferenceBookData(val id: String?, val name: String, val aspectId: String)
+data class ReferenceBookData(val id: String?, val name: String, val aspectId: String, val version: Int)
 
 @Serializable
-data class ReferenceBookItemData(val id: String?, val value: String, val parentId: String, val aspectId: String)
+data class ReferenceBookItemData(
+    val id: String?,
+    val value: String,
+    val parentId: String,
+    val aspectId: String,
+    val version: Int = 0
+)
