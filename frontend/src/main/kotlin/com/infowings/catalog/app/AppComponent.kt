817bee4f2478c63e24d7b11de05ee547870ddcd5
yaburtsev <YBurtsev@luxoft.com>
2018-04-05 16:23:24 +0300

    KS-52-units-page-aspects-page CatalogAppComponent refactoring: extract renderFunction

--- frontend/src/main/kotlin/com/infowings/catalog/app/AppComponent.kt
+++ frontend/src/main/kotlin/com/infowings/catalog/app/AppComponent.kt
@@ -3,9 +3,10 @@ package com.infowings.catalog.app
 import com.infowings.catalog.aspects.AspectsPage
 import com.infowings.catalog.auth.AuthComponent
 import com.infowings.catalog.auth.privateRoute
-import com.infowings.catalog.subjects.SubjectsPage
-import com.infowings.catalog.reference.book.ReferenceBookPage
 import com.infowings.catalog.measures.MeasuresPage
+import com.infowings.catalog.reference.book.ReferenceBookPage
+import com.infowings.catalog.subjects.SubjectsPage
+import com.infowings.catalog.wrappers.RouteSuppliedProps
 import com.infowings.catalog.wrappers.reactRouter
 import react.RBuilder
 import react.RComponent
@@ -22,46 +23,24 @@ class CatalogAppComponent : RComponent<RProps, RState>() {
                     component = ::AuthComponent
                 }
             }
-            privateRoute(
-                "/aspects",
-                renderFunction = { props ->
-                    child(AspectsPage::class) {
-                        attrs {
-                            location = props.location; history = props.history; match = props.match
-                        }
-                    }
-                })
-            privateRoute(
-                "/measures",
-                renderFunction = { props ->
-                    child(MeasuresPage::class) {
-                        attrs {
-                            location = props.location; history = props.history; match = props.match
-                        }
-                    }
-                })
-            privateRoute(
-                "/reference",
-                renderFunction = { props ->
-                    child(ReferenceBookPage::class) {
-                        attrs {
-                            location = props.location; history = props.history; match = props.match
-                        }
-                    }
-                })
-            privateRoute(
-                "/subjects",
-                renderFunction = { props ->
-                    child(SubjectsPage::class) {
-                        attrs {
-                            location = props.location; history = props.history; match = props.match
-                        }
-                    }
-                })
+            privateRoute("/aspects", renderFunction<AspectsPage>())
+            privateRoute("/measures", renderFunction<MeasuresPage>())
+            privateRoute("/reference", renderFunction<ReferenceBookPage>())
+            privateRoute("/subjects", renderFunction<SubjectsPage>())
             reactRouter.Route {
                 attrs {
                     path = "/"
                     render = { reactRouter.Redirect { attrs.to = "/aspects" } }
+                }
+            }
+        }
+    }
+
+    private inline fun <reified T : RComponent<RouteSuppliedProps, out RState>> renderFunction(): RBuilder.(RouteSuppliedProps) -> Unit {
+        return { props ->
+            child(T::class) {
+                attrs {
+                    location = props.location; history = props.history; match = props.match
                 }
             }
         }
