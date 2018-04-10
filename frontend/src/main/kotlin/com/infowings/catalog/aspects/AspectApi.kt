8f64bf82af52b9af37ef5fb51b720aca5de5f51c
yaburtsev <YBurtsev@luxoft.com>
2018-04-10 15:22:23 +0300

    KS-86-measure-search-bug-fix display 2 columns: units and measures if these groups not empty

--- frontend/src/main/kotlin/com/infowings/catalog/aspects/AspectApi.kt
+++ frontend/src/main/kotlin/com/infowings/catalog/aspects/AspectApi.kt
@@ -2,6 +2,7 @@ package com.infowings.catalog.aspects
 
 import com.infowings.catalog.common.AspectData
 import com.infowings.catalog.common.AspectsList
+import com.infowings.catalog.common.SuggestedMeasureData
 import com.infowings.catalog.utils.encodeURIComponent
 import com.infowings.catalog.utils.get
 import com.infowings.catalog.utils.post
@@ -23,5 +24,5 @@ suspend fun getSuggestedAspects(query: String, aspectId: String?, aspectProperty
     return JSON.parse(get("/api/search/aspect/suggestion?text=$query&aspectId=$aspectIdEncoded&aspectPropertyId=$propertyAspectIdEncoded"))
 }
 
-suspend fun getSuggestedMeasurementUnits(query: String, findInGroups: Boolean = false): Array<String> =
-    kotlin.js.JSON.parse(get("/api/search/measure/suggestion?text=$query&findInGroups=$findInGroups"))
+suspend fun getSuggestedMeasureData(query: String, findInGroups: Boolean = false): SuggestedMeasureData =
+    JSON.parse(get("/api/search/measure/suggestion?text=$query&findInGroups=$findInGroups"))
