f21ff2c3436abb3d99ead93fb002c485fe4862b0
yakov.burtsev <yakovburtsev@gmail.com>
2018-04-24 19:56:55 +0300

    Revert "KS-91-users-page-frontend UsersPage"
    This reverts commit 9cfa43c

--- frontend/src/main/kotlin/com/infowings/catalog/layout/Header.kt
+++ frontend/src/main/kotlin/com/infowings/catalog/layout/Header.kt
@@ -55,14 +55,6 @@ class Header : RComponent<Header.Props, RState>() {
                         text = buildElement { +"History" }
                     }
                 }
-                Button {
-                    attrs {
-                        className = "pt-minimal"
-                        active = props.location == "/users"
-                        onClick = { props.history.push("/users") }
-                        text = buildElement { +"Users" }
-                    }
-                }
             }
             NavbarGroup {
                 attrs.align = Alignment.RIGHT
