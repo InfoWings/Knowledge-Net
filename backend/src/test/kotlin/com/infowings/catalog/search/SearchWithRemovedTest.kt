79853dd0ef7dd8dca0d9ff2f3c8d131249210eb2
artemijbezguzikov <artemii.bezguzikov@gmail.com>
2018-06-04 16:04:33 +0300

    Tests fix

--- backend/src/test/kotlin/com/infowings/catalog/search/SearchWithRemovedTest.kt
+++ backend/src/test/kotlin/com/infowings/catalog/search/SearchWithRemovedTest.kt
@@ -1,11 +1,11 @@
 package com.infowings.catalog.search
 
 import com.infowings.catalog.MasterCatalog
-import com.infowings.catalog.common.AspectData
-import com.infowings.catalog.common.AspectPropertyData
-import com.infowings.catalog.common.Metre
-import com.infowings.catalog.common.Tonne
-import com.infowings.catalog.data.aspect.*
+import com.infowings.catalog.common.*
+import com.infowings.catalog.data.aspect.Aspect
+import com.infowings.catalog.data.aspect.AspectDaoService
+import com.infowings.catalog.data.aspect.AspectService
+import com.infowings.catalog.data.aspect.toAspectVertex
 import com.infowings.catalog.storage.OrientDatabase
 import com.infowings.catalog.storage.session
 import com.orientechnologies.orient.core.record.OVertex
