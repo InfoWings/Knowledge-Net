56505e4691417566d074aa2a136da73eeb3fafd0
Sergey Reznick <sr@air-consulting.ru>
2018-06-26 18:59:52 +0300

    KS-203: even more detailed time stamp logs

--- backend/src/main/kotlin/com/infowings/catalog/data/aspect/AspectOVertexExtensions.kt
+++ backend/src/main/kotlin/com/infowings/catalog/data/aspect/AspectOVertexExtensions.kt
@@ -154,10 +154,19 @@ class AspectVertex(private val vertex: OVertex) : HistoryAware, OVertex by verte
         "AspectVertex[id=${this.id}, name=${this.name}]"
 
     private fun toAspectOnlyData(): AspectData {
+        val baseType = logTime(logger, "extracting measure name") { baseType }
         val baseTypeObj = baseType?.let { BaseType.restoreBaseType(it) }
         val subjectData = logTime(logger, "extracting subject data") { subject?.toSubjectData() }
         val refBookVertex = logTime(logger, "extracting refBook vertex") {vertex.getVertices(ODirection.OUT, ASPECT_REFERENCE_BOOK_EDGE)}
         val refBookValue = logTime(logger, "extracting refBook value") { referenceBookRootVertex?.value }
+        val id = logTime(logger, "extracting aspect id") {id}
+        val name = logTime(logger, "extracting aspect name") {name}
+        val measureName = logTime(logger, "extracting measure name") {measureName}
+        val version = logTime(logger, "extracting version") {version}
+        val deleted = logTime(logger, "extracting deleted flag") {deleted}
+        val description = logTime(logger, "extracting description") {description}
+        val lastChange = logTime(logger, "extracting last change") {lastChange}
+
         return AspectData(
             id = id,
             name = name,
