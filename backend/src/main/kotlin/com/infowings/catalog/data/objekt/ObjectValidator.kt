7be65928478fe2dff086dc1d9522f6cb245fda69
kochkr <kochkar.roman@gmail.com>
2018-07-23 15:33:34 +0300

    KS-236 - Object removal frontend - removed commented code (extra business key check)

--- backend/src/main/kotlin/com/infowings/catalog/data/objekt/ObjectValidator.kt
+++ backend/src/main/kotlin/com/infowings/catalog/data/objekt/ObjectValidator.kt
@@ -152,21 +152,12 @@ class ObjectValidator(
 
         val objectPropertyVertex = objectService.findPropertyById(objPropertyVertex.id)
         val aspectPropertyVertex = valueVertex.aspectProperty
-                //?: throw IllegalStateException("ObjectPropertyValue ${valueVertex.id} has no linked Aspect") // TODO: Remove commented code
 
         val parentValueVertex = valueVertex.parentValue
 
         val dataValue = request.value
 
         val value = getObjectValueFromData(dataValue)
-
-        // check business key // TODO: Not a business key. Remove commented code
-//        val existsSameValue = objectDaoService.getValuesByObjectPropertyAndValue(objPropertyVertex.identity, value).any {
-//            it.id != valueVertex.id
-//        }
-//        if (existsSameValue) {
-//            throw ObjectPropertyValueAlreadyExists(value.toObjectValueData())
-//        }
 
         return ValueWriteInfo(
             value,
