5c9ea27912a03279fb0647d29f25ae41f0cee23f
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2018-08-07 23:31:57 +0300

    #276 Improve test performance

--- backend/src/test/kotlin/com/infowings/catalog/data/aspect/AspectServiceSavingTest.kt
+++ backend/src/test/kotlin/com/infowings/catalog/data/aspect/AspectServiceSavingTest.kt
@@ -13,14 +13,13 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import kotlin.test.assertEquals
 
 
 @RunWith(SpringJUnit4ClassRunner::class)
 @SpringBootTest(classes = [MasterCatalog::class])
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class AspectServiceSavingTest {
     private val username = "admin"
 
@@ -40,12 +39,13 @@ class AspectServiceSavingTest {
 
     @Test
     fun testAddAspect() {
-        val ad = AspectData("", "newAspect", Kilometre.name, null, Decimal.name, emptyList())
+        val name = "testAddAspect-newAspect"
+        val ad = AspectData("", name, Kilometre.name, null, Decimal.name, emptyList())
         val createAspect: AspectData = aspectService.save(ad, username)
 
         assertThat(
             "aspect should be saved and restored",
-            aspectService.findByName("newAspect").firstOrNull(),
+            aspectService.findByName(name).firstOrNull(),
             Is.`is`(createAspect)
         )
     }
@@ -69,30 +69,32 @@ class AspectServiceSavingTest {
 
     @Test
     fun testAddAspectWithEmptyParams() {
-        val ad = AspectData("", "newAspect", null, null, Decimal.name, emptyList())
+        val name = "testAddAspectWithEmptyParams-newAspect"
+        val ad = AspectData("", name, null, null, Decimal.name, emptyList())
         val createAspect: AspectData = aspectService.save(ad, username)
 
         assertThat(
             "aspect should be saved and restored event when some params are missing",
-            aspectService.findByName("newAspect").firstOrNull(),
+            aspectService.findByName(name).firstOrNull(),
             Is.`is`(createAspect)
         )
     }
 
     @Test(expected = AspectInconsistentStateException::class)
     fun testAddAspectWithEmptyParams2() {
-        val ad = AspectData("", "newAspect", null, null, null, emptyList())
+        val ad = AspectData("", "testAddAspectWithEmptyParams2-newAspect", null, null, null, emptyList())
         aspectService.save(ad, username)
     }
 
     @Test
     fun testAddAspectWithEmptyParams3() {
-        val ad = AspectData("", "newAspect", Kilometre.name, null, Decimal.name, emptyList())
+        val name = "testAddAspectWithEmptyParams3-newAspect"
+        val ad = AspectData("", name, Kilometre.name, null, Decimal.name, emptyList())
         val createAspect: AspectData = aspectService.save(ad, username)
 
         assertThat(
             "aspect should be saved and restored event when some params are missing",
-            aspectService.findByName("newAspect").firstOrNull(),
+            aspectService.findByName(name).firstOrNull(),
             Is.`is`(createAspect)
         )
     }
@@ -101,7 +103,7 @@ class AspectServiceSavingTest {
     fun testFailAddAspect() {
         val ad = AspectData(
             "",
-            "newAspect",
+            "testFailAddAspect-newAspect",
             Kilometre.name,
             OpenDomain(Boolean).toString(),
             BaseType.Boolean.name,
@@ -112,10 +114,10 @@ class AspectServiceSavingTest {
 
     @Test(expected = AspectAlreadyExist::class)
     fun testAddTwoAspectsSameName() {
-        val ad = AspectData("", "aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
+        val ad = AspectData("", "testAddTwoAspectsSameName-aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
         aspectService.save(ad, username)
 
-        val ad2 = AspectData("", "aspect", Metre.name, null, Decimal.name, emptyList(), 1)
+        val ad2 = AspectData("", "testAddTwoAspectsSameName-aspect", Metre.name, null, Decimal.name, emptyList(), 1)
         aspectService.save(ad2, username)
 
         assertThat("should return two aspects with name 'aspect'", aspectService.findByName("aspect").size, Is.`is`(2))
@@ -123,49 +125,49 @@ class AspectServiceSavingTest {
 
     @Test(expected = AspectAlreadyExist::class)
     fun testAddTwoAspectsSameNameIgnoreCase() {
-        val ad = AspectData("", "aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
+        val ad = AspectData("", "testAddTwoAspectsSameNameIgnoreCase-aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
         aspectService.save(ad, username)
 
-        val ad2 = AspectData("", "Aspect", Metre.name, null, Decimal.name, emptyList(), 1)
+        val ad2 = AspectData("", "testAddTwoAspectsSameNameIgnoreCase-Aspect", Metre.name, null, Decimal.name, emptyList(), 1)
         aspectService.save(ad2, username)
     }
 
     @Test(expected = AspectAlreadyExist::class)
     fun testAddAspectsSameNameSameMeasure() {
-        val ad1 = AspectData("", "aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
+        val ad1 = AspectData("", "testAddAspectsSameNameSameMeasure-aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
         aspectService.save(ad1, username)
 
-        val ad2 = AspectData("", "aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
+        val ad2 = AspectData("", "testAddAspectsSameNameSameMeasure-aspect", Kilometre.name, null, BaseType.Decimal.name, emptyList())
         aspectService.save(ad2, username)
     }
 
     @Test
     fun testAddAspectsDiffNameSameMeasure() {
-        val ad1 = AspectData("", "aspect", Kilometre.name, null, Decimal.name, emptyList())
+        val ad1 = AspectData("", "testAddAspectsDiffNameSameMeasure-aspect", Kilometre.name, null, Decimal.name, emptyList())
         aspectService.save(ad1, username)
 
-        val ad2 = AspectData("", "aspect2", Kilometre.name, null, Decimal.name, emptyList())
+        val ad2 = AspectData("", "testAddAspectsDiffNameSameMeasure-aspect2", Kilometre.name, null, Decimal.name, emptyList())
         aspectService.save(ad2, username)
 
         assertThat(
             "should return two aspects with same measure",
-            aspectService.getAspects().filter { it.measure == Kilometre.name }.size,
+            aspectService.getAspects().filter { it.measure == Kilometre.name }.filter { it.name.startsWith("testAddAspectsDiffNameSameMeasure") }.size,
             Is.`is`(2)
         )
     }
 
     @Test(expected = AspectInconsistentStateException::class)
     fun testUnCorrectMeasureBaseTypeRelations() {
-        val ad = AspectData("", "aspect", Kilometre.name, null, BaseType.Boolean.name, emptyList())
+        val ad = AspectData("", "testUnCorrectMeasureBaseTypeRelations-aspect", Kilometre.name, null, BaseType.Boolean.name, emptyList())
         aspectService.save(ad, username)
     }
 
     @Test
     fun testAspectWithoutCyclicDependency() {
-        val aspect = prepareAspect()
+        val aspect = prepareAspect("testAspectWithoutCyclicDependency")
         assertThat(
             "aspect should be saved and restored if no cyclic dependencies",
-            aspectService.findByName("aspect").firstOrNull(),
+            aspectService.findByName("testAspectWithoutCyclicDependency-aspect").firstOrNull(),
             Is.`is`(aspect)
         )
     }
@@ -173,7 +175,7 @@ class AspectServiceSavingTest {
 
     @Test(expected = AspectCyclicDependencyException::class)
     fun testAspectCyclicDependency() {
-        val aspect = prepareAspect()
+        val aspect = prepareAspect("testAspectCyclicDependency")
         val editedPropertyData1 = AspectPropertyData("", "prop1", aspect.idStrict(), PropertyCardinality.INFINITY.name, null)
         val aspect1 = aspectService.findById(aspect.properties.first().aspectId)
         val editedAspectData1 = AspectData(
@@ -191,7 +193,7 @@ class AspectServiceSavingTest {
 
     @Test
     fun testDoubleSaveNoSubject() {
-        val aspectData = aspectDataWithSubject("AspectDoubleSaveNoSubject")
+        val aspectData = aspectDataWithSubject("testDoubleSaveNoSubject-AspectDoubleSaveNoSubject")
         aspectService.save(aspectData, username)
         try {
             aspectService.save(aspectData, username)
@@ -203,25 +205,22 @@ class AspectServiceSavingTest {
         }
     }
 
-    @Test
+    @Test(expected = AspectAlreadyExist::class)
     fun testDoubleSaveSameSubject() {
-        val aspectData = aspectDataWithSubject("AspectDoubleSaveSameSubject", "SubjectDoubleSaveSameSubject")
+        val aspectData = aspectDataWithSubject(
+            aspectName = "testDoubleSaveSameSubject-AspectDoubleSaveSameSubject",
+            subjectName = "testDoubleSaveSameSubject-SubjectDoubleSaveSameSubject"
+        )
         aspectService.save(aspectData, username)
-        try {
-            aspectService.save(aspectData, username)
-            Assert.fail("Nothing thrown")
-        } catch (e: AspectAlreadyExist) {
-        } catch (e: Throwable) {
-            Assert.fail("Thrown unexpected $e")
-        }
+        aspectService.save(aspectData, username)
     }
 
     @Test
     fun testDoubleSaveWithAndWithoutSubject() {
-        val aspectData1 = aspectDataWithSubject("AspectDoubleSaveWithAndWithoutSubject")
+        val aspectData1 = aspectDataWithSubject("testDoubleSaveWithAndWithoutSubject-AspectDoubleSaveWithAndWithoutSubject")
         val aspectData2 = aspectDataWithSubject(
-            "AspectDoubleSaveWithAndWithoutSubject",
-            "SubjectDoubleSaveWithAndWithoutSubject"
+            aspectName = "testDoubleSaveWithAndWithoutSubject-AspectDoubleSaveWithAndWithoutSubject",
+            subjectName = "testDoubleSaveWithAndWithoutSubject-SubjectDoubleSaveWithAndWithoutSubject"
         )
 
         val aspect1 = aspectService.save(aspectData1, username)
@@ -229,7 +228,8 @@ class AspectServiceSavingTest {
 
         Assert.assertEquals("first subject is incorrect", null, aspect1.subject)
         Assert.assertEquals(
-            "second subject is incorrect", aspectData2.subject?.copy(version = 2),
+            "second subject is incorrect",
+            aspectData2.subject?.copy(version = 2),
             aspect2.subject
         )
     }
@@ -237,8 +237,8 @@ class AspectServiceSavingTest {
     @Test
     fun testDoubleSaveWithoutAndWithSubject() {
         val aspectData1 = aspectDataWithSubject(
-            "AspectDoubleSaveWithoutAndWithSubject",
-            "SubjectDoubleSaveWithoutAndWithSubject"
+            aspectName = "testDoubleSaveWithoutAndWithSubject-AspectDoubleSaveWithoutAndWithSubject",
+            subjectName = "testDoubleSaveWithoutAndWithSubject-SubjectDoubleSaveWithoutAndWithSubject"
         )
         val aspectData2 = aspectDataWithSubject("AspectDoubleSaveWithoutAndWithSubject")
 
@@ -255,12 +255,12 @@ class AspectServiceSavingTest {
     @Test
     fun testDoubleSaveDifferentSubjects() {
         val aspectData1 = aspectDataWithSubject(
-            "AspectDoubleSaveDifferentSubjects",
-            "SubjectDoubleSaveDifferentSubjects"
+            aspectName = "testDoubleSaveDifferentSubjects-AspectDoubleSaveDifferentSubjects",
+            subjectName = "testDoubleSaveDifferentSubjects-SubjectDoubleSaveDifferentSubjects"
         )
         val aspectData2 = aspectDataWithSubject(
-            "AspectDoubleSaveDifferentSubjects",
-            "OtherSubjectDoubleSaveDifferentSubjects"
+            aspectName = "testDoubleSaveDifferentSubjects-AspectDoubleSaveDifferentSubjects",
+            subjectName = "testDoubleSaveDifferentSubjects-OtherSubjectDoubleSaveDifferentSubjects"
         )
 
         val aspect1 = aspectService.save(aspectData1, username)
@@ -278,14 +278,14 @@ class AspectServiceSavingTest {
 
     @Test
     fun testTripleSaveNoSomeOther() {
-        val aspectData1 = aspectDataWithSubject("AspectTripleSaveNoSomeOther")
+        val aspectData1 = aspectDataWithSubject("testTripleSaveNoSomeOther-AspectTripleSaveNoSomeOther")
         val aspectData2 = aspectDataWithSubject(
-            "AspectTripleSaveNoSomeOther",
-            "SomeSubjectTripleSaveNoSomeOther"
+            aspectName = "testTripleSaveNoSomeOther-AspectTripleSaveNoSomeOther",
+            subjectName = "testTripleSaveNoSomeOther-SomeSubjectTripleSaveNoSomeOther"
         )
         val aspectData3 = aspectDataWithSubject(
-            "AspectTripleSaveNoSomeOther",
-            "OtherSubjectTripleSaveNoSomeOther"
+            aspectName = "testTripleSaveNoSomeOther-AspectTripleSaveNoSomeOther",
+            subjectName = "testTripleSaveNoSomeOther-OtherSubjectTripleSaveNoSomeOther"
         )
 
         val aspect1 = aspectService.save(aspectData1, username)
@@ -306,13 +306,13 @@ class AspectServiceSavingTest {
     @Test
     fun testTripleSaveSomeNoOther() {
         val aspectData1 = aspectDataWithSubject(
-            "AspectTripleSaveSomeNoOther",
-            "SomeSubjectTripleSaveSomeNoOther"
+            aspectName = "testTripleSaveSomeNoOther-AspectTripleSaveSomeNoOther",
+            subjectName = "testTripleSaveSomeNoOther-SomeSubjectTripleSaveSomeNoOther"
         )
-        val aspectData2 = aspectDataWithSubject("AspectTripleSaveSomeNoOther")
+        val aspectData2 = aspectDataWithSubject("testTripleSaveSomeNoOther-AspectTripleSaveSomeNoOther")
         val aspectData3 = aspectDataWithSubject(
-            "AspectTripleSaveSomeNoOther",
-            "OtherSubjectTripleSaveSomeNoOther"
+            aspectName = "testTripleSaveSomeNoOther-AspectTripleSaveSomeNoOther",
+            subjectName = "testTripleSaveSomeNoOther-OtherSubjectTripleSaveSomeNoOther"
         )
 
         val aspect1 = aspectService.save(aspectData1, username)
@@ -333,12 +333,12 @@ class AspectServiceSavingTest {
     @Test
     fun testTripleSaveSomeOtherNo() {
         val aspectData1 = aspectDataWithSubject(
-            "AspectTripleSaveSomeOtherNo",
-            "SomeSubjectTripleSaveSomeOtherNo"
+            aspectName = "testTripleSaveSomeOtherNo-AspectTripleSaveSomeOtherNo",
+            subjectName = "testTripleSaveSomeOtherNo-SomeSubjectTripleSaveSomeOtherNo"
         )
         val aspectData2 = aspectDataWithSubject(
-            "AspectTripleSaveSomeOtherNo",
-            "OtherSubjectTripleSaveSomeOtherNo"
+            aspectName = "testTripleSaveSomeOtherNo-AspectTripleSaveSomeOtherNo",
+            subjectName = "testTripleSaveSomeOtherNo-OtherSubjectTripleSaveSomeOtherNo"
         )
         val aspectData3 = aspectDataWithSubject("AspectTripleSaveSomeOtherNo")
 
@@ -376,19 +376,24 @@ class AspectServiceSavingTest {
 
     @Test
     fun testSaveWithAroundSpaces() {
-        val leafAspect = aspectService.save(AspectData(name = "leaf", baseType = BaseType.Text.name), username)
+        val leafAspect = aspectService.save(AspectData(name = "testSaveWithAroundSpaces-leaf", baseType = BaseType.Text.name), username)
         val aspectPropertyData =
             AspectPropertyData(id = "", name = "   p1   ", aspectId = leafAspect.id!!, cardinality = PropertyCardinality.ONE.name, description = "  d1   ")
         val complexAspect =
-            AspectData(name = "     test ", description = " description    ", baseType = BaseType.Text.name, properties = listOf(aspectPropertyData))
+            AspectData(
+                name = "     testSaveWithAroundSpaces-test ",
+                description = " description    ",
+                baseType = BaseType.Text.name,
+                properties = listOf(aspectPropertyData)
+            )
         val res = aspectService.save(complexAspect, username)
-        assertEquals(res.name, "test", "Aspect should save with trimmed name")
+        assertEquals(res.name, "testSaveWithAroundSpaces-test", "Aspect should save with trimmed name")
         assertEquals(res.description, "description", "Aspect should save with trimmed description")
         assertEquals(res.properties[0].name, "p1", "Aspect property should save with trimmed name")
         assertEquals(res.properties[0].description, "d1", "Aspect property should save with trimmed description")
     }
 
-    private fun prepareAspect(): AspectData {
+    private fun prepareAspect(testName: String): AspectData {
         /*
          *  aspect
          *    aspectProperty
@@ -397,15 +402,15 @@ class AspectServiceSavingTest {
          *              aspect2
          */
 
-        val aspectData2 = AspectData(null, "aspect2", Kilogram.name, null, Decimal.name, emptyList())
+        val aspectData2 = AspectData(null, "$testName-aspect2", Kilogram.name, null, Decimal.name, emptyList())
         val aspect2: AspectData = aspectService.save(aspectData2, username)
 
         val aspectPropertyData1 = AspectPropertyData("", "prop1", aspect2.idStrict(), PropertyCardinality.INFINITY.name, null)
-        val aspectData1 = AspectData(null, "aspect1", Metre.name, null, Decimal.name, listOf(aspectPropertyData1))
+        val aspectData1 = AspectData(null, "$testName-aspect1", Metre.name, null, Decimal.name, listOf(aspectPropertyData1))
         val aspect1: AspectData = aspectService.save(aspectData1, username)
 
         val aspectPropertyData = AspectPropertyData("", "prop", aspect1.idStrict(), PropertyCardinality.INFINITY.name, null)
-        val aspectData = AspectData(null, "aspect", Metre.name, null, Decimal.name, listOf(aspectPropertyData))
+        val aspectData = AspectData(null, "$testName-aspect", Metre.name, null, Decimal.name, listOf(aspectPropertyData))
         return aspectService.save(aspectData, username)
     }
 
