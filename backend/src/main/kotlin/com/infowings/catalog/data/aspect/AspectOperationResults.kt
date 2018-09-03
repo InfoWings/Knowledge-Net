0db11e626389d492eda1847ecfec2783257a5922
kochkr <kochkar.roman@gmail.com>
2018-09-03 19:42:50 +0300

    #250, #287, #288 - Suppressed Detekt warning

--- backend/src/main/kotlin/com/infowings/catalog/data/aspect/AspectOperationResults.kt
+++ backend/src/main/kotlin/com/infowings/catalog/data/aspect/AspectOperationResults.kt
@@ -1,6 +1,6 @@
+@file:Suppress("MatchingDeclarationName") // TODO: Remove @Suppress when more classes will be in the file
 package com.infowings.catalog.data.aspect
 
-@Suppress("MatchingDeclarationName")
 data class AspectPropertyDeleteResult(
     val aspectPropertyVertex: AspectPropertyVertex,
     val parentAspectVertex: AspectVertex,
