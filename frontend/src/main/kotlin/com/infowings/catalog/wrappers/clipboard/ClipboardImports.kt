ac3c524f925605ca098b99588eeab74935366ba7
kochkr <kochkar.roman@gmail.com>
2018-09-10 18:16:30 +0300

    #251 - Copy entity GUID button - Detekt warning suppress

--- frontend/src/main/kotlin/com/infowings/catalog/wrappers/clipboard/ClipboardImports.kt
+++ frontend/src/main/kotlin/com/infowings/catalog/wrappers/clipboard/ClipboardImports.kt
@@ -4,6 +4,7 @@ import org.w3c.dom.Element
 import org.w3c.dom.events.Event
 
 @JsModule("clipboard")
+@Suppress("UnusedPrivateMember")
 external class ClipboardJS(selector: String, options: ClipboardOptions = definedExternally) {
     fun on(name: String, fn: (Event) -> Unit)
     fun destroy()
