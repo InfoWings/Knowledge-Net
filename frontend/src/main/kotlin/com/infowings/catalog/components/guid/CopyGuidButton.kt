e84beec5235d7d1ff95013dd96468cfdd69860e2
kochkr <kochkar.roman@gmail.com>
2018-09-10 15:10:39 +0300

    #251 - Copy entity GUID button - possibility of empty GUID

--- frontend/src/main/kotlin/com/infowings/catalog/components/guid/CopyGuidButton.kt
+++ frontend/src/main/kotlin/com/infowings/catalog/components/guid/CopyGuidButton.kt
@@ -1,18 +1,36 @@
 package com.infowings.catalog.components.guid
 
+import com.infowings.catalog.wrappers.blueprint.Button
+import com.infowings.catalog.wrappers.blueprint.Intent
 import com.infowings.catalog.wrappers.blueprint.Tooltip
 import com.infowings.catalog.wrappers.react.asReactElement
 import kotlinx.html.ButtonType
 import react.RBuilder
 import react.dom.button
 
-fun RBuilder.copyGuidButton(text: String) {
+private const val GUID_NOT_PROVIDED_PLACEHOLDER = "Guid is not provided"
+
+fun RBuilder.copyGuidButton(text: String?) {
     Tooltip {
-        attrs {
-            content = text.asReactElement()
-        }
-        button(type = ButtonType.button, classes = "pt-button pt-minimal pt-small pt-icon-clipboard copy-entity-guid-btn") {
-            setProp("data-clipboard-text", text)
+        text?.let {
+            attrs {
+                content = text.asReactElement()
+            }
+            button(type = ButtonType.button, classes = "pt-button pt-minimal pt-small pt-icon-clipboard copy-entity-guid-btn") {
+                setProp("data-clipboard-text", text)
+            }
+        } ?: run {
+            attrs {
+                content = GUID_NOT_PROVIDED_PLACEHOLDER.asReactElement()
+            }
+            Button {
+                attrs {
+                    className = "pt-minimal"
+                    icon = "clipboard"
+                    intent = Intent.DANGER
+                    disabled = true
+                }
+            }
         }
     }
 }
